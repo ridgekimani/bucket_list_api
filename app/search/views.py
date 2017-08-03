1491372d621022a1549fbe2c60df10c12f18d57e
Ridge Kimani <ridgekimani@gmail.com>
2017-08-04 00:35:37 +0300

    Working search api

--- app/search/views.py
+++ app/search/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, request
+from app.models import Bucket, Activity
+from flask import Blueprint, make_response, jsonify, request
 
 
 search = Blueprint('search', __name__, url_prefix='/search')
@@ -7,3 +8,11 @@ search = Blueprint('search', __name__, url_prefix='/search')
 @search.route('/', methods=['GET'])
 def search_api():
     query = request.args.get('q')
+    if not query:
+        return make_response(jsonify(error='Please enter search parameters'), 400)
+
+    bucket_lists = Bucket.query.filter_by(bucket_name=str(query).capitalize()).all()
+    activities = Activity.query.filter_by(description=str(query).capitalize()).all()
+    bucket_obj = [bucket.serialize for bucket in bucket_lists]
+    activity_obj = [activity.serialize for activity in activities]
+    return make_response(jsonify(buckets=bucket_obj, activities=activity_obj))
