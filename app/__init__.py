5ba30877c48f2957e277f13d3b949981383040a0
Ridge Kimani <ridgekimani@gmail.com>
2017-08-06 14:29:15 +0300

    Added 404 handler

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
 
 from settings.settings import app_config
 
@@ -9,6 +9,11 @@ app.config.from_object(app_config['development'])
 from app.auth.views import auth
 from app.bucketlists.views import bucketlist
 from app.search.views import search
+
+
+@app.errorhandler(400)
+def not_found():
+    return make_response(jsonify(dict(error='Resource not found')), 404)
 
 
 app.register_blueprint(auth)
